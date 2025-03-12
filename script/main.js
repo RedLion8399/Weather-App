f124b3fefa5ba80c235ec3553d185e8aba50acc3
Paul Jonas <Paul.Jonas@dohle-net.de>
2025-03-12 18:41:54 +0100

    refactor(main.ts): refactor API calls
    - Use async functions instead of promises
    - Integrate secound API Key for locationq
    - Load API Keys to global vars and seperate this call from calling the API
    - Replace 'console.error' by throw new Error
    - Seperate path from base URL for API call and add path later
        This ensures that different API calls can be calld using the same function later
    - Fix misleaing type annotations of lat and long

--- script/main.js
+++ script/main.js
@@ -1,49 +1,80 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let weatherKey;
+let locationKey;
 const weatherDisplay = document.getElementById("weather-display");
-fetch("secrets.json")
-    .then((response) => response.json())
-    .then((data) => {
-    let key = data.apiKey.trim();
-    if (!key) {
-        throw new Error("API Key not found. Please save OpenWeatherMap-API Key to secrets.txt");
-    }
-    getWeather(key);
-})
-    .catch((err) => {
-    console.error(`${err} Please save OpenWeatherMap-API Key to secrets.txt in root directory.`);
-    return;
-});
+function loadApiKey() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let response = yield fetch("secrets.json");
+            let data = yield response.json();
+            weatherKey = data.weatherKey.trim();
+            locationKey = data.locationKey.trim();
+        }
+        catch (err) {
+            throw new Error(`${err} - Please save API-Keys to secrets.txt in root directory.`);
+        }
+        if (!weatherKey) {
+            throw new Error("API Key not found. Please save OpenWeatherMap-API Key to secrets.txt");
+        }
+        if (!locationKey) {
+            throw new Error("API Key not found. Please save locationiq-API Key to secrets.txt");
+        }
+    });
+}
 function displayWeather(weatherData) {
     weatherDisplay.innerHTML = weatherData;
 }
-function getWeather(apiKey) {
-    let lat, lon;
-    lat = 52.52;
-    lon = 13.41;
-    const RequestUrl = new URL("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={ApiKey}");
-    // The free plan offers three different APIs:
-    // - https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-    // - api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}
-    // - api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-    RequestUrl.searchParams.set("lat", lat.toString());
-    RequestUrl.searchParams.set("lon", lon.toString());
-    RequestUrl.searchParams.set("appid", apiKey);
-    RequestUrl.searchParams.set("units", "metric");
-    RequestUrl.searchParams.set("lang", "de");
-    let response;
-    fetch(RequestUrl)
-        .then((response) => {
+function buildRequestUrl() {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!weatherKey) {
+            yield loadApiKey();
+        }
+        let lat;
+        let lon;
+        lat = 51.44488;
+        lon = 8.34851;
+        let baseUrl = "https://api.openweathermap.org";
+        const requestUrl = new URL(baseUrl);
+        // The free plan offers three different APIs:
+        // - https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+        // - api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}
+        // - api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+        requestUrl.searchParams.set("lat", lat.toString());
+        requestUrl.searchParams.set("lon", lon.toString());
+        requestUrl.searchParams.set("appid", weatherKey);
+        requestUrl.searchParams.set("units", "metric");
+        requestUrl.searchParams.set("lang", "de");
+        requestUrl.pathname = "data/2.5/weather";
+        // requestUrl.pathname = "data/2.5/forecast/daily";
+        // requestUrl.pathname = "data/2.5/forecast";
+        return requestUrl;
+    });
+}
+function getWeather() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let requestUrl = yield buildRequestUrl();
+        let response;
+        try {
+            response = yield fetch(requestUrl);
+        }
+        catch (err) {
+            throw new Error(`${err} - API Request failed`);
+        }
         if (!response.ok) {
             throw new Error(response.statusText);
         }
-        return response.json();
-    })
-        .then((data) => {
-        response = data;
-        console.log(response);
-        displayWeather(JSON.stringify(response));
-    })
-        .catch((err) => {
-        console.log(err);
+        let data = yield response.json();
+        return JSON.stringify(data);
     });
 }
+loadApiKey();
+getWeather().then((weatherData) => displayWeather(weatherData));
--- script/main.ts
+++ script/main.ts
@@ -1,62 +1,86 @@
+let weatherKey: string;
+let locationKey: string;
+
 const weatherDisplay = document.getElementById("weather-display");
 
-fetch("secrets.json")
-  .then((response) => response.json())
-  .then((data) => {
-    let key: string = data.apiKey.trim();
-    if (!key) {
-      throw new Error(
-        "API Key not found. Please save OpenWeatherMap-API Key to secrets.txt"
-      );
-    }
+async function loadApiKey(): Promise<void> {
+  interface Secrets {
+    weatherKey: string;
+    locationKey: string;
+  }
 
-    getWeather(key);
-  })
-  .catch((err) => {
-    console.error(
-      `${err} Please save OpenWeatherMap-API Key to secrets.txt in root directory.`
+  try {
+    let response: Response = await fetch("secrets.json");
+    let data: Secrets = await response.json();
+    weatherKey = data.weatherKey.trim();
+    locationKey = data.locationKey.trim();
+  } catch (err) {
+    throw new Error(
+      `${err} - Please save API-Keys to secrets.txt in root directory.`
     );
-    return;
-  });
+  }
+  if (!weatherKey) {
+    throw new Error(
+      "API Key not found. Please save OpenWeatherMap-API Key to secrets.txt"
+    );
+  }
+  if (!locationKey) {
+    throw new Error(
+      "API Key not found. Please save locationiq-API Key to secrets.txt"
+    );
+  }
+}
 
 function displayWeather(weatherData: string): void {
   weatherDisplay.innerHTML = weatherData;
 }
 
-function getWeather(apiKey: string): void {
-  let lat, lon: number;
-  lat = 52.52;
-  lon = 13.41;
+async function buildRequestUrl(): Promise<URL> {
+  if (!weatherKey) {
+    await loadApiKey();
+  }
 
-  const RequestUrl: URL = new URL(
-    "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={ApiKey}"
-  );
+  let lat: number;
+  let lon: number;
+  lat = 51.44488;
+  lon = 8.34851;
+
+  let baseUrl: string = "https://api.openweathermap.org";
+  const requestUrl: URL = new URL(baseUrl);
+
   // The free plan offers three different APIs:
   // - https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
   // - api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}
   // - api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-  RequestUrl.searchParams.set("lat", lat.toString());
-  RequestUrl.searchParams.set("lon", lon.toString());
-  RequestUrl.searchParams.set("appid", apiKey);
-  RequestUrl.searchParams.set("units", "metric");
-  RequestUrl.searchParams.set("lang", "de");
+  requestUrl.searchParams.set("lat", lat.toString());
+  requestUrl.searchParams.set("lon", lon.toString());
+  requestUrl.searchParams.set("appid", weatherKey);
+  requestUrl.searchParams.set("units", "metric");
+  requestUrl.searchParams.set("lang", "de");
 
-  let response: JSON;
+  requestUrl.pathname = "data/2.5/weather";
+  // requestUrl.pathname = "data/2.5/forecast/daily";
+  // requestUrl.pathname = "data/2.5/forecast";
 
-  fetch(RequestUrl)
-    .then((response): Promise<JSON> => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then((data): void => {
-      response = data;
-      console.log(response);
-      displayWeather(JSON.stringify(response));
-    })
-    .catch((err): void => {
-      console.log(err);
-    });
+  return requestUrl;
 }
+
+async function getWeather(): Promise<string> {
+  let requestUrl: URL = await buildRequestUrl();
+
+  let response: Response;
+  try {
+    response = await fetch(requestUrl);
+  } catch (err) {
+    throw new Error(`${err} - API Request failed`);
+  }
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  let data = await response.json();
+  return JSON.stringify(data);
+}
+
+loadApiKey();
+getWeather().then((weatherData: string) => displayWeather(weatherData));
