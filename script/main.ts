25240c9e2c06f33aaf20077481034de100195bbc
Paul Jonas <Paul.Jonas@dohle-net.de>
2025-03-15 11:53:25 +0100

    feat(main.ts): add ForecastWeather interface for extended weather data structure

--- script/main.ts
+++ script/main.ts
@@ -94,6 +94,40 @@ interface CurrentWeather {
   name: string;
 }
 
+interface ForecastWeather {
+  cnt: number;
+  list: [
+    dt: number,
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      sea_level: number;
+      grnd_level: number;
+      humidity: number;
+    },
+    weather: [main: string, description: string, icon: string],
+    clouds: { all: number },
+    wind: { speed: number; deg: number; gust: number },
+    visibility: number,
+    pop: number,
+    rain: { "3h": number },
+    sys: { pod: string },
+    dt_txt: string
+  ];
+
+  city: {
+    name: string;
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
+
 async function getWeather(
   lat: number = 51.44488,
   lon: number = 8.34851
